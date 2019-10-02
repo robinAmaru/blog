9b86bccb82a3cca0da2dcb2ef76bf31fc08b3c76
Robin AMARU <r.amaru@groupeonepoint.com>
2019-10-02 09:08:30 +0200

    ajout commentaire

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
+  // définition des input attendus
   @Input() postTitre: string;
   @Input() postContent: string;
   @Input() postLoveIts: number;
