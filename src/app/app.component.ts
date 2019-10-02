0168f8d051f4c769f9e8142369f2dca9fbfc086f
Robin AMARU <r.amaru@groupeonepoint.com>
2019-10-02 09:18:48 +0200

    ajout de la branche post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+// initialisation de la date
+  datetoday = new Date();
+
+// création du tableau posts
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. ',
+      loveIts: 0,
+      dateCreated: this.datetoday
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper.',
+      loveIts: 0,
+      dateCreated: this.datetoday
+        },
+    {
+      title: 'Encore un post',
+      content: 'Lorem ipsum dolor sit a digni, adipisci, dolor. Cis arcu massa, scelerisque vitae, consequat in, pretium a, enim. ',
+      loveIts: 0,
+      dateCreated: this.datetoday
+    }
+];
+
+
 }
