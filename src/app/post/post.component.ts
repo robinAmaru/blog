93233db2e6f14afcbcf81354c7d710df922a908d
Robin AMARU <r.amaru@groupeonepoint.com>
2019-10-02 08:43:47 +0200

    ajout affichage des posts

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input() postTitre: string;
+  @Input() postContent: string;
+  @Input() postLoveIts: number;
+  @Input() postCreated: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onLove(num: number)  {
+    this.postLoveIts =  this.postLoveIts + num;
+   }
+
+  getColor() {
+    if (this.postLoveIts > 0) {
+      return 'green';
+    } else if (this.postLoveIts < 0) {
+      return 'red';
+     } else if (this.postLoveIts === 0) {
+      return 'black';
+     }
+    }
+
+}
